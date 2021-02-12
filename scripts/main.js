8db10d23faeab511fc3b2ffda2ae2f4bc19ebba1
wilsjame <jgwilson1214@gmail.com>
2021-02-11 21:26:25 -0800

    add some javascript ☕️🖋
    - Change image on click.
    - Button to change name.

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/lisa-simpson.jpg') {
+        myImage.setAttribute('src','images/lisa-simpson.png');
+    } else {
+        myImage.setAttribute('src','images/lisa-simpson.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter a name.');
+    if(!myName) {
+        myHeading.textContent = "Lisa Simpson";
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = `${myName} Simpson`;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = `${storedName} Simpson`;
+
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
